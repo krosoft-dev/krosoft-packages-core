1bf3b1c492e10d0cb3c3907fc9dff5b6ee8f62bc
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-03-28 02:08:24 +0100

    Fix hexToRgba

--- src/helpers/color.helper.ts
+++ src/helpers/color.helper.ts
@@ -1,5 +1,5 @@
 export function hexToRgba(hex: string | null | undefined, alpha: number): string {
-  if (hex == null || hex === "" || hex.length < 7) return `rgba(0,16,49,${String(alpha / 100)})`;
+  if (hex === null || hex === undefined || hex === "" || hex.length < 7) return `rgba(0,16,49,${String(alpha / 100)})`;
   const r = parseInt(hex.slice(1, 3), 16);
   const g = parseInt(hex.slice(3, 5), 16);
   const b = parseInt(hex.slice(5, 7), 16);
